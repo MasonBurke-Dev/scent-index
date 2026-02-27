6c8e310b4440c796d8cdd90e2743fb6791106022
MRB76000 <mb840705@gmail.com>
2026-02-27 11:48:59 -0500

    first commit

---
+++ src/test/java/com/example/cologne_app/CologneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cologne_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CologneAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
